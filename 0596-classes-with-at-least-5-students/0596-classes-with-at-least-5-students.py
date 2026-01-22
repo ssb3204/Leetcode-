db4feb78005e8c3b39ed3ae4f51b8700b7909ef4
ryan(SON) <ryanp3@naver.com>
2026-01-22 17:05:55 +0900

    Time: 325 ms (5.05%), Space: 67.9 MB (80.83%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
-    df=courses.copy()
-    cnt_df = df["class"].value_counts().reset_index(name="cnt")
+
+    cnt_df = courses["class"].value_counts().reset_index(name="cnt")
+    cnt_df = cnt_df.rename(columns={"index":"class"})
     return cnt_df[cnt_df["cnt"]>=5][["class"]]
