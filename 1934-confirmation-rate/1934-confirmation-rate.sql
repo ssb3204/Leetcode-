c9f259ea1e3a8b48fccd89fb6a6e3947da4c4c22
ryan(SON) <ryanp3@naver.com>
2026-01-30 15:45:57 +0900

    Time: 604 ms (74.65%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select s.user_id, round(avg(if(c.action='confirmed',1,0)),2) confirmation_rate
+from signups s left join confirmations c on s.user_id=c.user_id
+group by s.user_id
